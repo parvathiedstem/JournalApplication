5ca8634787818f4e51a79478242b696331a46e5e
parvathi k <parvathi@parvathis-MacBook-Air.local>
2023-11-06 17:31:11 +0530

    minor changes

--- src/main/java/com/Eudaimonia/DigitalJournalApp/DigitalJournalAppApplication.java
+++ src/main/java/com/Eudaimonia/DigitalJournalApp/DigitalJournalAppApplication.java
@@ -7,17 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
 
 @SpringBootApplication
-//@EnableSwagger2
 public class DigitalJournalAppApplication {
 
 	public static void main(String[] args) {
@@ -32,24 +25,6 @@ public class DigitalJournalAppApplication {
 				.setFieldAccessLevel(Configuration.AccessLevel.PRIVATE);
 		return mapper;
 	}
-
-	@Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.Eudaimonia.DigitalJournalApp"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(metaData());
-    }
-
-    private ApiInfo metaData() {
-        return new ApiInfoBuilder()
-                .title("DigitalJournalApp")
-                .description("\"Swagger configuration for application\"")
-                .version("1.1.0")
-                .build();
-    }
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
