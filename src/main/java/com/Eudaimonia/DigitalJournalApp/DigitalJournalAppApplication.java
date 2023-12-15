8a47fde5c47ae7473a15cc07389cbcd09a6b2b9f
parvathi k <parvathi@parvathis-MacBook-Air.local>
2023-12-15 12:52:05 +0530

    minor change

--- src/main/java/com/Eudaimonia/DigitalJournalApp/DigitalJournalAppApplication.java
+++ src/main/java/com/Eudaimonia/DigitalJournalApp/DigitalJournalAppApplication.java
@@ -25,14 +25,4 @@ public class DigitalJournalAppApplication {
 				.setFieldAccessLevel(Configuration.AccessLevel.PRIVATE);
 		return mapper;
 	}
-//	@Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/journal")
-//						.allowedOrigins("http://127.0.0.1:5173/").allowedHeaders("Content-Type");
-//			}
-//		};
-//	}
 }
